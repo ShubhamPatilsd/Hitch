e206b52850e633dc173ac4d9bc8fbd162eadf8be
Shubham <shubhampatilsd@gmail.com>
2022-09-24 12:36:23 -0700

    tailwind

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Space Grotesk", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: { DEFAULT: "#5f27cd", darker: "#341f97" },
+      },
+    },
   },
   plugins: [],
 };
