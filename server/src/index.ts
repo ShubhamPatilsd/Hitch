a24dcbff5608b1c6ef9e39495199ea4a8a32bf48
Shubham <shubhampatilsd@gmail.com>
2022-09-24 11:58:22 -0700

    express

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at https://localhost:${port}`);
+});
